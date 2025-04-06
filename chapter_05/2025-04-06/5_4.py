ff4eb7c42685cf5f60b06e6411edf792ff95c62b
Juan D. Bravo <196306536+juanbravo-dev@users.noreply.github.com>
2025-04-06 18:29:42 +0200

    Add exercise 5-4: if-else for alien color points

---
+++ chapter_05/2025-04-06/5_4.py
@@ -0,0 +1,33 @@
+
+# Simple, only if if
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("The player just earned 5 points")
+
+if alien_color != "green":
+    print("The player just earned 10 points")
+
+
+# Running the else block
+
+alien_color = "red"
+
+if alien_color == "green":
+    print("The player just earned 5 points")
+
+else:
+    print("The player just earned 10 points")
+
+
+# Running the if block
+
+alien_color = "green"
+
+if alien_color == "green":
+    print("The player just earned 5 points")
+
+else:
+    print("The player just earned 10 points")
+
